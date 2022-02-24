6f6e7a2f56adda9f2d146c1beee9ff2142aa59ee
Marcin <m.niemirski@time4.digital>
2022-02-24 20:10:21 +0100

    Add crud methods to recipes

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,38 +1,108 @@
 const express = require('express')
 const router = express.Router()
 const Recipe = require('../models/recipe')
+const { body, validationResult } = require('express-validator')
 
 router.get('/', async (req, res) => {
   let recipes
 
   try {
-    recipes = await Recipe.find().sort({ createAt: 'desc' }).limit(10).exec()
+    recipes = await Recipe.find().sort({ createdAt: 'desc' })
+    res.status(200).json({ recipes })
   } catch {
     recipes = []
   }
-
-  console.log(recipes)
 })
 
-router.post('/add', async (req, res) => {
-  const { name, author, ingredients, protein, carbohydrates, fat, calories } = req.body
+// Adding new recipe
+router.post(
+  '/add',
+  body('name').isString(),
+  body('author').isMongoId(),
+  body('ingredients').isArray(),
+  body('protein').isNumeric(),
+  body('carbohydrates').isNumeric(),
+  body('fat').isNumeric(),
+  body('calories').isNumeric(),
+  async (req, res) => {
+    const { name, author, ingredients, protein, carbohydrates, fat, calories } = req.body
 
-  const recipe = new Recipe({
-    name,
-    author,
-    ingredients,
-    protein,
-    carbohydrates,
-    fat,
-    calories,
-  })
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+
+    if (!name || !author || !ingredients || !protein || !carbohydrates || !fat || !calories) {
+      throw new Error('missing parametrs')
+    }
+
+    const recipe = new Recipe({
+      name,
+      author,
+      ingredients,
+      protein,
+      carbohydrates,
+      fat,
+      calories,
+    })
+
+    try {
+      const newRecipe = await recipe.save()
+      res.status(200).json({ newRecipe })
+    } catch (err) {
+      res.status(500)
+      throw new Error('recipe add error')
+    }
+  },
+)
+
+router.put('/edit', body('id').isMongoId(), async (req, res) => {
+  const { id, name, author, ingredients, protein, carbohydrates, fat, calories } = req.body
+
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() })
+  }
+
+  if (!name || !author || !ingredients || !protein || !carbohydrates || !fat || !calories) {
+    throw new Error('missing parametrs')
+  }
+
+  let recipe
 
   try {
-    const newRecipe = await recipe.save()
-    res.json({ newRecipe })
+    recipe = await Recipe.findById(id)
+    recipe.name = name
+    recipe.author = author
+    recipe.ingredients = ingredients
+    recipe.protein = protein
+    recipe.carbohydrates = carbohydrates
+    recipe.fat = fat
+    recipe.calories = calories
+
+    const editedRecipe = await recipe.save()
+    res.status(200).json({ editedRecipe })
+  } catch (err) {
+    res.status(500)
+    throw new Error('recipe edit error')
+  }
+})
+
+router.delete('/delete', body('id').isMongoId(), async (req, res) => {
+  const { id } = req.body
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() })
+  }
+
+  let recipe
+  try {
+    recipe = await Recipe.findById(id)
+    await recipe.remove()
+    res.status(200)
   } catch {
-    throw new Error('recipe error')
-    // renderNewPage(res, book, true)
+    res.status(500)
+    throw new Error('recipe edit error')
   }
 })
 
